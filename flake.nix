0bd7437154a666b7178c8b1953bac9e858737bd9
zimbatm <zimbatm@zimbatm.com>
2020-08-31 15:01:27 +0200

    remove nixpkgs from the flake
    Since nixpkgs is used only during development, don't expose it to Flake.
    This avoids having to pull yet another instance of nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,29 @@
 {
   description = "mkDevShell";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self }:
     let
-      devshell = import ./shell.nix {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      # nixpkgs is only used for development. Don't add it to the flake.lock.
+      gitRev = "500d695aac9ea67195812f309890a911fbc96bda";
+      nixpkgs = builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/${gitRev}.tar.gz";
+        sha256 = "sha256-ya3rCWKDbPWMVsh89/Z1mCJ9HFa5/DKdjcgcKkWB1xs=";
       };
+
+      # Real developers use Linux for development :-p
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs { inherit system; };
+
+      devshell = import ./shell.nix { inherit pkgs; };
     in
     {
+      # Import this overlay into your instance of nixpkgs
       overlay = import ./overlay.nix;
-      defaultApp.x86_64-linux = devshell.flakeApp;
-      devShell.x86_64-linux = devshell;
+
+      ## Only use for development
+      defaultPackage.${system} = devshell;
+      defaultApp.${system} = devshell.flakeApp;
+      devShell.${system} = devshell;
     };
 }
