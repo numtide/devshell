54d8814ce037aee7efb261b9605e879ef202dfb2
zimbatm <zimbatm@zimbatm.com>
2020-10-08 00:05:29 +0200

    flake.nix: remove dead code

--- flake.nix
+++ flake.nix
@@ -3,19 +3,9 @@
 
   outputs = { self }:
     let
-      # nixpkgs is only used for development. Don't add it to the flake.lock.
-      gitRev = "500d695aac9ea67195812f309890a911fbc96bda";
-      nixpkgs = builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/${gitRev}.tar.gz";
-        sha256 = "sha256-ya3rCWKDbPWMVsh89/Z1mCJ9HFa5/DKdjcgcKkWB1xs=";
-      };
-
       # Real developers use Linux for development :-p
       system = "x86_64-linux";
-
-      pkgs = import ./. {
-        inherit nixpkgs system;
-      };
+      pkgs = import ./. { inherit system; };
     in
     {
       # Import this overlay into your instance of nixpkgs
