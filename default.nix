448430075b56b8417baf6b9a7996a86484ecbe89
David Arnold <dar@xoe.solutions>
2020-10-09 16:07:33 -0500

    Provide means to add extra overlays to devhsell

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { nixpkgs ? import ./nix/nixpkgs.nix
 , system ? builtins.currentSystem
+, overlays ? []
 }:
 import nixpkgs {
   inherit system;
-  overlays = [ (import ./overlay.nix) ];
+  overlays = [ (import ./overlay.nix) ] ++ overlays;
 }
