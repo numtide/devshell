7f4627160baf577a3c8869da40df97593cb73a74
zimbatm <zimbatm@zimbatm.com>
2020-10-10 22:53:36 +0000

    ci: add goreleaser (#34)

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/numtide/devshell/cmd"
 )
 
+// These values are automatically set by `goreleaser` or can be set with
+// `go build -ldflags="-X main.version=something"` for example.
+var (
+// version = "dev"
+// commit  = "unknown"
+// date    = "unknown"
+// builtBy = "unknown"
+)
+
 func main() {
 	err := cmd.Execute()
 	if err != nil {
