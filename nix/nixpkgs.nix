a35fa6bb51358c1c4fb20f877525f7f720ca168b
zimbatm <zimbatm@zimbatm.com>
2021-01-03 12:33:54 +0100

    nix: update nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   # nixpkgs is only used for development. Don't add it to the flake.lock.
-  gitRev = "500d695aac9ea67195812f309890a911fbc96bda";
+  gitRev = "733e537a8ad76fd355b6f501127f7d0eb8861775";
 in
 builtins.fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/${gitRev}.tar.gz";
-  sha256 = "sha256-ya3rCWKDbPWMVsh89/Z1mCJ9HFa5/DKdjcgcKkWB1xs=";
+  sha256 = "1rjvbycd8dkkflal8qysi9d571xmgqq46py3nx0wvbzwbkvzf7aw";
 }
