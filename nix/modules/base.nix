58005c10814470265006569a255766bc7b43f051
zimbatm <zimbatm@zimbatm.com>
2021-01-04 23:10:52 +0100

    base: no need to create a submodule

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -164,29 +164,22 @@ in
       '';
     };
 
-    bash = mkOption {
-      type = types.submodule {
-        options = {
-          extra = mkOption {
-            type = types.lines;
-            default = "";
-            description = ''
-              Extra commands to run in bash on environment startup.
-            '';
-          };
+    bash.extra = mkOption {
+      type = types.lines;
+      default = "";
+      description = ''
+            Extra commands to run in bash on environment startup.
+      '';
+    };
 
-          interactive = mkOption {
-            type = types.lines;
-            default = "";
-            description = ''
+    bash.interactive = mkOption {
+      type = types.lines;
+      default = "";
+      description = ''
               Same as shellHook, but is only executed on interactive shells.
 
               This is useful to setup things such as custom prompt commands.
-            '';
-          };
-        };
-      };
-      default = { };
+      '';
     };
 
     env = mkOption {
