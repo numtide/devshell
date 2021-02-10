1c16db26875a9cf7f207be3f541fefe8c6574a4a
Jonas Chevalier <zimbatm@zimbatm.com>
2021-02-10 21:37:13 +0000

    modules: introduce [[devshell.env]] (#77)
    It's a more powerful version of [env] that supports more options.

--- tests/devshell.nix
+++ tests/devshell.nix
@@ -1,7 +1,7 @@
 { pkgs, devshell }:
 {
-  # Basic devshell usage
-  devshell-1 =
+  # Basic devshell package usage
+  devshell-packages-1 =
     let
       shell = devshell.mkShell {
         devshell.name = "devshell-1";
@@ -20,6 +20,51 @@
 
       # Adds packages to the PATH
       type -p git
+
+      touch $out
+    '';
+
+  # Test the environment variables
+  devshell-env-1 =
+    let
+      shell = devshell.mkShell {
+        devshell.name = "devshell-env-1";
+        devshell.env = [
+          {
+            name = "HTTP_PORT";
+            value = 8080;
+          }
+          {
+            name = "PATH";
+            prefix = "bin";
+          }
+          {
+            name = "XDG_CACHE_DIR";
+            eval = "$DEVSHELL_ROOT/$(echo .cache)";
+          }
+        ];
+      };
+    in
+    pkgs.runCommand "devshell-env-1" { } ''
+      unset XDG_DATA_DIRS
+
+      source ${./assert.sh}
+
+      # Load the devshell
+      source ${shell}
+
+      # NIXPKGS_PATH is being set
+      assert "$NIXPKGS_PATH" == "${toString pkgs.path}"
+
+      assert "$XDG_DATA_DIRS" == "$DEVSHELL_DIR/share:/usr/local/share:/usr/share"
+
+      assert "$HTTP_PORT" == 8080
+
+      # PATH is prefixed with an expanded bin folder
+      [[ $PATH == $PWD/bin:* ]]
+
+      # Eval
+      assert "$XDG_CACHE_DIR" == "$PWD/.cache"
 
       touch $out
     '';
