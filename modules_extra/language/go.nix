16e667ed43eb435fed7bcd03791edf6b37c879e1
Jonas Chevalier <zimbatm@zimbatm.com>
2021-02-13 14:18:30 +0000

    introduce language.go (#88)
    This is our first language support!
    
    When "language.go" is imported, it adds the go package to the
    environment and sets GO111MODULE.
    
    This changes the extra modules import rule a little bit. Now that other
    prefixes than `extra.` is being used, the modules doc will have to be
    split in two to remove confusion on what is part of the core modules and
    what is part of the extra modules.

---
+++ modules_extra/language/go.nix
@@ -0,0 +1,31 @@
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.language.go;
+  strOrPackage = import ../../nix/strOrPackage.nix { inherit lib pkgs; };
+in
+with lib;
+{
+  options.language.go = {
+    GO111MODULE = mkOption {
+      type = types.enum [ "on" "off" "auto" ];
+      default = "on";
+      description = "Enable Go modules";
+    };
+
+    package = mkOption {
+      type = strOrPackage;
+      default = pkgs.go;
+      example = literalExample "pkgs.go";
+      description = "Which go package to use";
+    };
+  };
+
+  config = {
+    env = [{
+      name = "GO111MODULE";
+      value = cfg.GO111MODULE;
+    }];
+
+    devshell.packages = [ cfg.package ];
+  };
+}
