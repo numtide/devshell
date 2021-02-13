efa4587fc1ade72adde2683ad91755fa664bd53c
zimbatm <zimbatm@zimbatm.com>
2021-02-13 17:49:19 +0100

    tests: make room for the extra modules

--- tests/default.nix
+++ tests/default.nix
@@ -11,12 +11,39 @@ let
 
       touch $out
     '';
+
+  # Arguments to pass to each test file
   attrs = { inherit pkgs devshell runTest; };
+
+  # Attrs marked with this attribute are recursed into by nix-build
+  recursive = { recurseForDerivations = true; };
+
+  loadDir = testPrefix: dir:
+    let
+      data = builtins.readDir dir;
+      op = sum: name:
+        let
+          path = "${dir}/${name}";
+          type = data.${name};
+          # Nix doesn't recurse into attrs that have dots in them...
+          attr = builtins.replaceStrings [ "." ] [ "-" ]
+            (pkgs.lib.removeSuffix ".nix" name);
+
+          args = attrs // {
+            # Customize runTest
+            runTest = name: runTest "${testPrefix}.${attr}.${name}";
+          };
+        in
+        assert type == "regular";
+        sum //
+        {
+          "${attr}" = recursive // (import path args);
+        }
+      ;
+    in
+    builtins.foldl' op recursive (builtins.attrNames data);
 in
-{ recurseForDerivations = true; }
-// (import ./commands.nix attrs)
-// (import ./devshell.nix attrs)
-// (import ./env.nix attrs)
-// (import ./git-hooks.nix attrs)
-// (import ./modules-docs.nix attrs)
-  // { }
+{
+  recurseForDerivations = true;
+  core = loadDir "tests.core" (toString ./core);
+}
