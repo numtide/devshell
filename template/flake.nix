13d4c0a1c2704b124f651f6144b454abb690cf0b
Timothy DeHerrera <tim.deh@pm.me>
2021-02-23 22:50:26 -0700

    template: remove indirection

--- template/flake.nix
+++ template/flake.nix
@@ -7,16 +7,14 @@
   outputs = { self, flake-utils, devshell, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShell =
-        let inherit (pkgs.devshell) mkShell importTOML;
+        let pkgs = import nixpkgs {
+          inherit system;
 
-          pkgs = import nixpkgs {
-            inherit system;
-
-            overlays = [ devshell.overlay ];
-          };
+          overlays = [ devshell.overlay ];
+        };
         in
-        mkShell {
-          imports = [ (importTOML ./devshell.toml) ];
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
         };
     });
 }
