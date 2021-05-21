f3f6a9cd7457bcb9a4464d0e076702ab6515f915
Pacman99 <pachum99@myrdd.info>
2021-05-20 21:41:49 -0700

    modules: add importTOML as a specialArg this would allow modules to import toml files in an `imports` line

--- modules/default.nix
+++ modules/default.nix
@@ -13,6 +13,7 @@ let
     modules = [ configuration ] ++ devenvModules;
     specialArgs = {
       modulesPath = builtins.toString ./.;
+      inherit (pkgs.devshell) importTOML;
     } // extraSpecialArgs;
   };
 in
