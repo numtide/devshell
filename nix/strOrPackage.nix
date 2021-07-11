7336fa86e7ca8842fb6ebd970e910fa5dba0b94e
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-07-11 04:03:12 +0200

    Allow `strOrPackage` type to be a configurable attribute set

--- nix/strOrPackage.nix
+++ nix/strOrPackage.nix
@@ -1,12 +1,46 @@
 { lib, pkgs }:
 with lib;
 let
-  resolveKey = key:
+  pkgOpt = {
+    pkg = mkOption {
+      type = types.package;
+      description = ''
+        The derivation of this package.
+      '';
+      exampleText = "pkgs.hello";
+    };
+    prio = mkOption = {
+      type = types.nullOr (types.either (types.enum [ "low" "high" ]) types.int);
+      description = ''
+        The priority of this package in the shell environment.
+
+        Left to default if null.
+      '';
+      default = null;
+      example = "high";
+    };
+  };
+
+  # Because we want to be able to push pure JSON-like data into the environment.
+  coerceStr = str:
     let
       attrs = builtins.filter builtins.isString (builtins.split "\\." key);
       op = sum: attr: sum.${attr} or (throw "package \"${key}\" not found");
     in
-    builtins.foldl' op pkgs attrs;
+    {
+      pkg = builtins.foldl' op pkgs attrs;
+    };
+
+  strOrAttrs =
+    types.coercedTo
+      types.str
+      coerceStr
+      (types.submodule { options = pkgOpt; });
+
+  coercePkgOpt = opt:
+    if isNull opt.prio then opt.pkg
+    else if opt.prio == "low" then lowPrio opt.pkg
+    else if opt.prio == "high" then hiPrio opt.pkg
+    else setPrio opt.prio opt.pkg;
 in
-# Because we want to be able to push pure JSON-like data into the environment.
-types.coercedTo types.str resolveKey types.package
+types.coercedTo strOrAttrs coercePkgOpt types.package
