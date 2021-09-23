b4bd83243a6bfda159b7663b02a208967b4901de
zimbatm <zimbatm@zimbatm.com>
2021-09-23 23:04:29 +0200

    extra: add service.postgres
    Automatically configure postgres when entering the project directory.
    
    Invoke `pg_ctl start` to start a server.

---
+++ extra/service/postgres.nix
@@ -0,0 +1,45 @@
+# This module automatically configures postgres when the user enters the
+# devshell.
+#
+# To start the server, invoke `postgres` in one devshell. Then start a second
+# devshell to run the clients.
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.service.postgres;
+in
+{
+  options.service.postgres = {
+    package = mkOption {
+      type = types.package;
+      description = "Which version of postgres to use";
+      default = pkgs.postgresql;
+      defaultText = "pkgs.postgresl";
+    };
+  };
+  config = {
+    packages = [ cfg.package ];
+
+    env = [
+      {
+        name = "PGDATA";
+        eval = "$PRJ_DATA_DIR/postgres";
+      }
+      {
+        name = "PGHOST";
+        eval = "$PGDATA";
+      }
+    ];
+
+    devshell.startup.setup-postgres.text = ''
+      if [[ ! -d "$PGDATA" ]]; then
+      initdb
+      cat >> "$PGDATA/postgresql.conf" <<EOF
+        listen_addresses = '''
+        unix_socket_directories = '$PGHOST'
+      EOF
+      echo "CREATE DATABASE ''${USER:-$(id -nu)};" | postgres --single -E postgres
+      fi
+    '';
+  };
+}
