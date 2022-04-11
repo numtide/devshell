6ec7953ba051654bc46c64b58085730f498fd9b2
Matt Schreiber <tomeon@dogea.red>
2022-04-11 07:40:12 -0400

    feat(hooks): support multiple worktrees (#180)
    by setting up git hook shim scripts that exec into the corresponding
    real hook scripts under ${DEVSHELL_GIT_HOOKS_DIR}/bin if and only
    if:
    
        1. DEVSHELL_DIR is defined,
        2. DEVSHELL_GIT_HOOKS_DIR is defined, and
        3. ${DEVSHELL_GIT_HOOKS_DIR}/bin/<hook-name> is executable.
    
    Otherwise, the hook shim scripts emit a diagnostic message and exit.
    
    This way, we can safely install a hook shim to the git hooks directory
    *even if the hook isn't active for all worktrees*, and punt to
    DEVSHELL_GIT_HOOKS_DIR to activate only those hooks that are pertinent
    for a given devshell.
    
    Ancillary changes:
    
        1. Respect GIT_COMMON_DIR on git clients that support it
           (GIT_COMMON_DIR may affect where git looks for hooks), and
        2. Respect the user's core.hooksPath setting.

--- extra/git/hooks.nix
+++ extra/git/hooks.nix
@@ -13,40 +13,56 @@ let
       };
     };
 
+  # All of the hook types supported by this module.
+  allHooks = filterAttrs (k: v: k != "enable") cfg;
+
   # Only keep all the hooks that have a value set.
-  hooksWithData = filterAttrs (k: v: k != "enable" && v.text != "") cfg;
+  hooksWithData = filterAttrs (k: v: v.text != "") allHooks;
+
+  # Shims for all the hooks that this module supports.  The shims cause git
+  # hooks to be ignored:
+  #
+  #   1. Outside of the devshell, or
+  #   2. When the current devshell doesn't define/enable *any* git hooks, or
+  #   3. When the current devshell doesn't define/enable the specific git hook
+  #      in question.
+  #
+  # The idea here is to support scenarios like switching between multiple git
+  # worktrees without having to reinstall the hook symlinks.  Instead, the hook
+  # shims read the correct "real" shim (directory) from DEVSHELL_GIT_HOOKS_DIR,
+  # which points to the directory containing git hooks for the current
+  # devshell.
+  hookShimsDir = pkgs.runCommand "git.hook.shims" {} ''
+    mkdir -p $out/bin
+
+    ${lib.concatMapStringsSep "\n" (k: ''
+    cat <<'WRAPPER' > $out/bin/${k}
+    #!${pkgs.bash}/bin/bash
+    set -euo pipefail
+
+    if [[ -z "''${DEVSHELL_DIR:-}" ]]; then
+      echo "${k}: ignoring git hook outside of devshell"; >&2
+      exit;
+    elif [[ -z "''${DEVSHELL_GIT_HOOKS_DIR:-}" ]]; then
+      echo "${k}: git hooks are not activated in this environment"; >&2
+      exit;
+    elif ! [[ -x "''${DEVSHELL_GIT_HOOKS_DIR}/bin/${k}" ]]; then
+      echo "${k}: the ${k} git hook is not activated in this environment"; >&2
+      exit;
+    fi
+
+    exec "''${DEVSHELL_GIT_HOOKS_DIR}/bin/${k}" "$@"
+    WRAPPER
+
+    # Mark as executable
+    chmod +x "$out/bin/${k}"
+    '') (builtins.attrNames allHooks)}
+  '';
 
   # A collection of all the git hooks in the /bin folder
   hooksDir =
     let
-      mkHookScript = k: hook:
-        pkgs.runCommand k
-          {
-            text = hook.text;
-            passAsFile = [ "text" ];
-          }
-          ''
-            mkdir -p $out/bin
-
-            cp "$textPath" "$out/bin/.${k}-wrapped"
-
-            # Add a wrapper so that the hooks are ignored outside of the
-            # devshell.
-            cat <<'WRAPPER' > $out/bin/${k}
-            #!${pkgs.bash}/bin/bash
-            set -euo pipefail
-
-            if [[ -z "''${DEVSHELL_DIR:-}" ]]; then
-              echo "${k}: ignoring git hook outside of devshell"; >&2
-              exit;
-            fi
-            exec "@out@/bin/.${k}-wrapped" "$@"
-            WRAPPER
-            sed -e "s|@out@|$out|g" -i "$out/bin/${k}"
-
-            # Mark as executable
-            chmod +x "$out/bin/.${k}-wrapped" "$out/bin/${k}"
-          '';
+      mkHookScript = k: hook: pkgs.writeShellScriptBin k hook.text;
     in
     pkgs.buildEnv {
       name = "git.hooks";
@@ -70,6 +86,10 @@ let
       fi
     }
 
+    git_path_absolute() {
+      ${pkgs.gitMinimal}/bin/git rev-parse --path-format=absolute "$@"
+    }
+
     # Add `readlink -f` for macOS
     export PATH=${pkgs.coreutils}/bin:$PATH
 
@@ -79,23 +99,20 @@ let
       log "skipping as we can't find any .git folder, we are probably not in a git repository" >&2
       exit
     fi
-    git_dir=$(${pkgs.gitMinimal}/bin/git rev-parse --absolute-git-dir)
-    source_hook_dir=${hooksDir}/bin
-    target_hook_dir=$git_dir/hooks
 
-    if [[ "$git_dir" != "$git_work_tree"/.git ]]; then
-      # There are cases where the '.git' folder lives in other places. For
-      # example the `git worktree` command. In these cases, don't touch the
-      # git hooks because they are shared between the various checkouts.
-      log "skipping as this worktree doen't contain the .git folder" >&2
-      exit
-    fi
+    # Respect GIT_COMMON_DIR on git clients that support it
+    git_dir=$(git_path_absolute --git-common-dir 2>/dev/null) || git_dir=$(git_path_absolute --git-dir)
+
+    source_hook_dir=${hookShimsDir}/bin
+
+    # Respect setups that define core.hooksPath
+    target_hook_dir=$(git_path_absolute --git-path hooks/ 2>/dev/null) || target_hook_dir=$git_dir/hooks
 
     # Just in case it doesn't exist
     mkdir -pv "$target_hook_dir"
 
-    # Iterate over all the hooks we know of
-    for name in ${toString (filter (name: name != "enable") (attrNames cfg))}; do
+    # Iterate over all the hooks enabled for this environment
+    for name in ${toString (attrNames hooksWithData)}; do
       # Resolve all the symlinks
       src_hook=$(readlink -f "$source_hook_dir/$name" || true)
       dst_hook=$(readlink -f "$target_hook_dir/$name" || true)
@@ -107,11 +124,6 @@ let
       elif [[ -f "$src_hook" ]]; then
         has_update
         ln -sfv "$src_hook" "$target_hook_dir/$name"
-      # If the target hook is a store path, assume it's an old hook and
-      # remove. Don't touch other existing hooks.
-      elif [[ "$dst_hook" == ${builtins.storeDir}/* ]]; then
-        has_update
-        rm -v "$target_hook_dir/$name"
       fi
     done
     if [[ $update != 0 ]]; then
@@ -147,5 +159,10 @@ in
     startup.install-git-hooks.text = "
       $DEVSHELL_DIR/bin/install-git-hooks
     ";
+  };
+
+  config.env = optional cfg.enable {
+    name = "DEVSHELL_GIT_HOOKS_DIR";
+    value = hooksDir;
   };
 }
