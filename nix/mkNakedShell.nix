f42776adabd9c768077a3a2516f270fb037f6a84
Gytis Ivaskevicius <me@gytis.io>
2022-09-16 17:55:29 +0300

    Add comment why 'name' variable is preserved

--- nix/mkNakedShell.nix
+++ nix/mkNakedShell.nix
@@ -45,6 +45,7 @@ in
     # Remove all the unnecessary noise that is set by the build env
     unset NIX_BUILD_TOP NIX_BUILD_CORES NIX_STORE
     unset TEMP TEMPDIR TMP TMPDIR
+    # $name variable is preserved to keep it compatible with pure shell https://github.com/sindresorhus/pure/blob/47c0c881f0e7cfdb5eaccd335f52ad17b897c060/pure.zsh#L235
     unset builder out shellHook stdenv system
     # Flakes stuff
     unset dontAddDisableDepTrack outputs
