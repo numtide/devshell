e3dc3e21594fe07bdb24bdf1c8657acaa4cb8f66
Gytis Ivaskevicius <gytis02.21@gmail.com>
2022-09-17 23:14:04 +0300

    Preserve derivation name (#221)
    * Preserve derivation name
    
    * Add comment why 'name' variable is preserved

--- nix/mkNakedShell.nix
+++ nix/mkNakedShell.nix
@@ -45,7 +45,8 @@ in
     # Remove all the unnecessary noise that is set by the build env
     unset NIX_BUILD_TOP NIX_BUILD_CORES NIX_STORE
     unset TEMP TEMPDIR TMP TMPDIR
-    unset builder name out shellHook stdenv system
+    # $name variable is preserved to keep it compatible with pure shell https://github.com/sindresorhus/pure/blob/47c0c881f0e7cfdb5eaccd335f52ad17b897c060/pure.zsh#L235
+    unset builder out shellHook stdenv system
     # Flakes stuff
     unset dontAddDisableDepTrack outputs
 
