ef97d3f9a8932a316171bf696d4c396c713f6a40
David Arnold <david.arnold@iohk.io>
2023-03-15 21:28:57 -0500

    refactor: make importTOML self-contained again

--- nix/importTOML.nix
+++ nix/importTOML.nix
@@ -1,34 +1,35 @@
-file:
-
-# Return a module that gets lib as an argument
-{ lib, ... }:
 let
-  importTOML = import ./importTOML.nix;
-  dir = toString (builtins.dirOf file);
-  data = builtins.fromTOML (builtins.readFile file);
-
-  extraModulesDir = toString ../extra;
-  extraModules = builtins.readDir extraModulesDir;
-
-  importModule = str:
+  importTOML = file:
+    # Return a module that gets lib as an argument
+    { lib, ... }:
     let
-      repoFile = "${dir}/${str}";
-      extraFile =
-        "${extraModulesDir}/${builtins.replaceStrings [ "." ] [ "/" ] str}.nix";
+      dir = toString (builtins.dirOf file);
+      data = builtins.fromTOML (builtins.readFile file);
+
+      extraModulesDir = toString ../extra;
+      extraModules = builtins.readDir extraModulesDir;
+
+      importModule = str:
+        let
+          repoFile = "${dir}/${str}";
+          extraFile =
+            "${extraModulesDir}/${builtins.replaceStrings [ "." ] [ "/" ] str}.nix";
+        in
+        # First try to import from the user's repository
+        if lib.hasPrefix "./" str || lib.hasSuffix ".nix" str || lib.hasSuffix ".toml" str
+        then
+          (
+            if lib.hasSuffix ".toml" str
+            then importTOML repoFile
+            else import repoFile
+          )
+        # Then fallback on the extra modules
+        else import extraFile;
     in
-    # First try to import from the user's repository
-    if lib.hasPrefix "./" str || lib.hasSuffix ".nix" str || lib.hasSuffix ".toml" str
-    then
-      (
-        if lib.hasSuffix ".toml" str
-        then importTOML repoFile
-        else import repoFile
-      )
-    # Then fallback on the extra modules
-    else import extraFile;
+    {
+      _file = file;
+      imports = map importModule (data.imports or [ ]);
+      config = builtins.removeAttrs data [ "imports" ];
+    };
 in
-{
-  _file = file;
-  imports = map importModule (data.imports or [ ]);
-  config = builtins.removeAttrs data [ "imports" ];
-}
+importTOML
