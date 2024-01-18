5a497495c4e248559722ac352620ff973824e95a
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-18 11:29:23 +0300

    refactor: interpolation test
    - fix test failing on macos due to directory name wrapping

--- tests/extra/devshell.menu.interpolate.nix
+++ tests/extra/devshell.menu.interpolate.nix
@@ -3,18 +3,25 @@
   interpolate =
     let
       shell = devshell.mkShell {
-        devshell.menu.interpolate = true;
-        commands = [
+        devshell.menu = {
+          interpolate = true;
+          width = 200;
+        };
+        commands.scripts = [
           { prefix = "hello"; help = ''hello from "$PRJ_ROOT"!''; }
           { prefix = "hola"; help = ''hola from '\$PRJ_ROOT'!''; }
+          { prefix = "hallo"; help = ''hallo from "$PRJ_ROOT"!''; interpolate = false; }
         ];
       };
     in
     runTest "interpolate" { } ''
       # Check interpolation is enabled
-      eval ${shell}/bin/menu | grep "hello from \"$PRJ_ROOT\"!"
+      ( eval ${shell}/bin/menu | grep "hello from \"$PRJ_ROOT\"!" )
       
       # Check escaped variable
-      eval ${shell}/bin/menu | grep 'hola from '\'''$PRJ_ROOT'\'
+      ( eval ${shell}/bin/menu | grep 'hola from '\'''$PRJ_ROOT'\' )
+
+      # Check non-interpolated variable
+      ( eval ${shell}/bin/menu | grep 'hallo from "$PRJ_ROOT"!' )
     '';
 }
