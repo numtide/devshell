b26808a620fe88cf01872c33384c4024e4798d14
Matt Schreiber <tomeon@dogea.red>
2024-03-06 13:11:58 -0500

    feat(templates): demo `extraModulesPath` usage
    in the `flake-parts` template.

--- templates/flake-parts/flake.nix
+++ templates/flake-parts/flake.nix
@@ -25,7 +25,25 @@
       perSystem =
         { pkgs, ... }:
         {
-          devshells.default = { };
+          devshells.default = (
+            { extraModulesPath, ... }@args:
+            {
+              # `extraModulesPath` provides access to additional modules that are
+              # not included in the standard devshell modules list.
+              #
+              # Please see https://numtide.github.io/devshell/extending.html for
+              # documentation on consuming extra modules, and see
+              # https://github.com/numtide/devshell/tree/main/extra for the
+              # extra modules that are currently available.
+              imports = [ "${extraModulesPath}/git/hooks.nix" ];
+
+              git.hooks.enable = false;
+              git.hooks.pre-commit.text = ''
+                echo 1>&2 'time to implement a pre-commit hook!'
+                exit 1
+              '';
+            }
+          );
         };
     };
 }
