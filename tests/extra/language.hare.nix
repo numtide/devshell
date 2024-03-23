99c62b409554f93619580fd450e21fa6e545d66b
Coutinho de Souza <dev@onemoresuza.mailer.me>
2024-03-23 07:32:18 -0300

    feat: add hare language support
    The module, besides providing the Hare binary through
    `devshell.packages`, exposes the following options:
    
    1. `thirdPartyLibs`: add hare third-party libraries
       (pkgs.hareThirdParty) to the HAREPATH environment variable.
    2. `vendoredLibs`: add vendored libraries (any path on the project repo)
       to HAREPATH.
    3. `package`: set which `hare` package to use (defaults to `pkgs.hare`).
    
    Regarding third-party libraries, the module also takes care of the ones
    included on `propagatedBuildInputs`, by adding them to HAREPATH.

---
+++ tests/extra/language.hare.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  devshell,
+  runTest,
+}: {
+  # Basic test
+  language-hare-1 = let
+    shell = devshell.mkShell {
+      imports = [../../extra/language/hare.nix];
+      devshell.name = "devshell-1";
+    };
+  in
+    runTest "language-hare-1" {} ''
+      # Load the devshell
+      source ${shell}/env.bash
+
+      # Has a Hare binary
+      type -p hare
+    '';
+  # Test good HAREPATH value
+  # TODO: When the nixpkgs input is updated, change hare-ev to hare-png, so
+  # that the inclusion of propagatedBuildInputs third-party libraries is also tested.
+  language-hare-2 = let
+    shell = devshell.mkShell {
+      imports = [../../extra/language/hare.nix];
+      devshell.name = "devshell-2";
+      language.hare = {
+        thirdPartyLibs = [pkgs.hareThirdParty.hare-compress];
+        vendoredLibs = ["./vendor/lib"];
+      };
+    };
+  in
+    runTest "language-hare-2" {} ''
+      # Load the devshell
+      source ${shell}/env.bash
+
+      die() {
+        printf -- '%s\n' "''${*}" 2>&1
+        printf -- 'HAREPATH: `%s`\n' ''${HAREPATH//:/ } 2>&1
+        exit 1
+      }
+
+      # Check for HAREPATH being set
+      [[ -n "$HAREPATH" ]] || die "HAREPATH not set"
+
+      # Check for the stdlib being included in HAREPATH
+      [[ "$HAREPATH" =~ "src/hare/stdlib" ]] || die 'HAREPATH lacks `stdlib`'
+
+      # Check for hare-ev being included in HAREPATH
+      [[ "$HAREPATH" =~ /nix/store/[a-z0-9]{32}-hare-compress-.*/src/hare/third-party ]] \
+        || die 'HAREPATH lacks `hare-compress`'
+
+      # Check for ./vendor/lib being included in HAREPATH
+      [[ "$HAREPATH" =~ $PWD/vendor/lib ]] || die "HAREPATH lacks \`$PWD/vendor/lib\`"
+    '';
+}
